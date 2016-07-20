e4aa53dea217e1deda1b3e1f64f6ad0c40161dae
Тимур <timur.starchenko@mail.ru>
2016-07-20 22:06:14 +0600

    Create toolbar and search button

--- RemidMe/app/src/main/java/com/example/remidme/MainActivity.java
+++ RemidMe/app/src/main/java/com/example/remidme/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.remidme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.inflateMenu(R.menu.menu);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
     }
 }
