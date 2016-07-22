fed8e5c5a1e9434a3f9d9eb146c1d1885a349dd1
Тимур <timur.starchenko@mail.ru>
2016-07-22 23:07:05 +0600

    Start to implementing Tabs

--- RemidMe/app/src/main/java/com/example/remidme/MainActivity.java
+++ RemidMe/app/src/main/java/com/example/remidme/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.remidme;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.example.remidme.adapter.TabsPagerFragmentAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drowerLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
+        intTabs();
     }
 
     private void initToolbar() {
@@ -29,5 +40,18 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void intTabs() {
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);   //связываем Tab с ViewPager
+    }
+
+    private void initNavigationView() {
+        drowerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
     }
 }
---
+++ RemidMe/app/src/main/java/com/example/remidme/adapter/TabsPagerFragmentAdapter.java
@@ -0,0 +1,44 @@
+package com.example.remidme.adapter;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.remidme.fragment.ExampleFragment;
+
+public class TabsPagerFragmentAdapter extends FragmentPagerAdapter{
+    private String[] tabs;
+
+    public TabsPagerFragmentAdapter(FragmentManager fm) {
+        super(fm);
+        tabs = new String[]{
+                "Tab 1",
+                "Напоминания",
+                "Tab 1"
+        };
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                break;
+            case 1:
+                return ExampleFragment.getInstance();
+            case 2:
+                break;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+}
